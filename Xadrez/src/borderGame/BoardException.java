811d4eb4d02698db0ba0729311ba0ed572f0d788
Gabriel <gabriel.pinheiro1240@gmail.com>
2023-11-08 14:02:38 -0300

    ChessException e ChessPosition

--- Xadrez/src/borderGame/BoardException.java
+++ Xadrez/src/borderGame/BoardException.java
@@ -2,7 +2,6 @@ package borderGame;
 
 public class BoardException extends RuntimeException {
     private static final long serialVersionUID = 1L;
-
     public BoardException(String msg){
         super(msg);
     }
